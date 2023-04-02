be0d50942d850ae2fb97f595f97fa4a0ad7f9403
efrenruizrubio <efren282@outlook.es>
2023-04-02 14:09:45 -0600

    :sparkles: Added a 'status' attribute to Resultado

--- controllers/results.js
+++ controllers/results.js
@@ -13,11 +13,58 @@ resultsRouter.get('/', async (request, response) => {
       patient: obj,
       score: r.Puntaje,
       testResults: JSON.parse(r.Respuesta),
-      appliedTest: r.TestAplicado
+      appliedTest: r.TestAplicado,
+      status: r.Estado
     }
   })
 
   response.status(200).json(results)
+})
+
+resultsRouter.post('/', async (request, response) => {
+  const { name, email, age, score, testResults, appliedTest, status } = request.body
+  console.log(request.body)
+  if (!name) {
+    return response.status(400).json({
+      error: 'The name of the patient is missing'
+    })
+  }
+  if (!email) {
+    return response.status(400).json({
+      error: 'The email of the patient is missing'
+    })
+  }
+  if (!age) {
+    return response.status(400).json({
+      error: 'The age of the patient is missing'
+    })
+  }
+  if (score === undefined) {
+    return response.status(400).json({
+      error: 'The score of the test is missing'
+    })
+  }
+  if (!testResults.length) {
+    return response.status(400).json({
+      error: 'The results of the test are missing'
+    })
+  }
+
+  const [patient] = await pool.query('SELECT * FROM Paciente WHERE Email = ?', email)
+  let patientId
+  if (!patient[0]) {
+    const [result] = await pool.query(`
+    INSERT INTO Paciente (Name, Email, Age, PsicologoId) VALUES (?, ?, ?, ?)
+    `, [name, email, age, 1])
+    patientId = result.insertId
+  } else {
+    patientId = patient[0].Id
+  }
+
+  await pool.query(`
+  INSERT INTO Resultado (Respuesta, PsicologoId, PacienteId, Puntaje, TestAplicado, Estado) VALUES (?, ?, ?, ?, ?, ?)
+  `, [JSON.stringify(testResults), 1, patientId, score, Number(appliedTest), status])
+  response.status(204).end()
 })
 
 module.exports = resultsRouter
